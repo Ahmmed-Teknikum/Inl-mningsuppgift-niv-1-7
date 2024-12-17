2594ec79710b4468b0a71adb608ccd6ed129d899
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-17 13:42:32 +0100

    Add project files.

---
+++ Inlämningsuppgift nivå 1 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InlämningsUppgift_._nivå1_._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummer;
+
+            Console.WriteLine("Skriv ett positivt tal. Om du vill avsluta så behöver du skriva ett negativt tal");
+
+            while (true)
+            {
+                Console.WriteLine("Ange ett tal: ");
+                nummer= int.Parse(Console.ReadLine());
+
+                if (nummer < 0)
+                {
+                    Console.WriteLine("Du har skrivit ett negativt tal. Programmet har avslutats");
+                    break;
+                }
+            }
+        }
+    }
+}
